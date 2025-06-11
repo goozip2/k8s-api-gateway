d68c217adcad6c2936f04eef0b71334efc16457b
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-06-11 14:01:17 +0900

    Initial Commit

---
+++ src/main/java/com/welab/k8s_api_gateway/common/exception/NotFound.java
@@ -0,0 +1,7 @@
+package com.welab.k8s_api_gateway.common.exception;
+
+public class NotFound extends ClientError {
+    public NotFound(String message) {
+        super("NotFound", message);
+    }
+}
